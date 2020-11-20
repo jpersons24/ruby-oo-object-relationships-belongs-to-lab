245faf479c519c402574fdad17de031609b31476
Jakob Persons <jakobep24@gmail.com>
2020-11-20 07:35:07 -0500

    finished artist, song, post and author

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,8 @@
-# songs should have title AND belong to artist
-# a song should be able to tell you the name of its artist
-
-
-
 class Artist 
 
-    attr_accessor :name
-
-    def initialize(name)
-        @name = name
-    end
+    attr_accessor :name 
 
 end
 
-artist = Artist.new("Drake")
 
-## committed not pushed
 
--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,8 @@
-require_relative 'artist.rb'
 require 'pry'
-
-# songs should have title AND belong to artist
-# a song should be able to tell you the name of its artist
-
-
 
 class Song
 
-    attr_accessor :title
-
-    def initialize(title)
-        @title = title
-    end
+    attr_accessor :title, :artist
 
 end
 
